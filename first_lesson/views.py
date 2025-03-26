17889ced431f8a3efebfdf7fd0dad3ebe660cb69
sofware-engeneer <kingradomir@gmail.com>
2025-03-26 23:54:18 +0600

    qwe

---
+++ first_lesson/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+#CBV
+
+class FirstLessonView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['text'] = "Привет я такой то и мы изучаем Джанго ура Товарищи"
+        context['emodji'] = "🤯"
+        context['run_string'] = "🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍"
+        return context
+
+# DEF 
+
+# def first_lesson(request):
+#   if request.method == 'GET':
+#     context = {
+#       "text": "Привет я такой то и мы изучаем Джанго ура Товарищи",
+#       "emodji": "🤯",
+#       "run_string": "🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍➡️🐍"
+#     }
